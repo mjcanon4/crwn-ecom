926696fd2bd6e830f8f951cc3bc6f08ba7cdd8d9
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-12-11 16:54:01 -0600

    first commit

--- .history/src/App_20211211163054.js
+++ .history/src/App_20211211163054.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./homepage.component";
-
 function App() {
-  return <HomePage />;
+  return <div>Hello World</div>;
 }
 
 export default App;
--- .history/src/App_20211211163723.js
+++ .history/src/App_20211211163723.js
@@ -4,7 +4,7 @@ import "./App.css";
 import HomePage from "./homepage.component";
 
 function App() {
-  return <HomePage />;
+  return <div>Hello World</div>;
 }
 
 export default App;
